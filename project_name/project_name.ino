b55434c669040dd99b9a328c0e3805893918f4a2
leo <leo.legay85@gmail.com>
2025-12-18 12:25:42 +0100

    test boutons

--- project_name/project_name.ino
+++ project_name/project_name.ino
@@ -1,21 +1,25 @@
 #include "Application.h"
-#include <LiquidCrystal.h>
 #include "LCD.h"
 
 Application myApplication;
+int etat = 0;
+int etat1 = 0;
 
 void setup() 
 {
   // put your setup code here, to run once:
-  myApplication.init();
-  //pinMode(0,OUTPUT);
-  //pinMode(5,INPUT);
-  //pinMode(12,OUTPUT);
+  //myApplication.init();
+  Serial.begin(9600);
+  pinMode(D7,OUTPUT);
+  pinMode(D5,INPUT);
+  Serial.println("led init");
 }
 
 void loop() 
 {
   // put your main code here, to run repeatedly:
-  myApplication.run();
-  
+  //myApplication.run();
+  etat = digitalRead(D5);
+  if (etat == HIGH) digitalWrite(D7, LOW);
+  else digitalWrite(D7,HIGH);
 }
