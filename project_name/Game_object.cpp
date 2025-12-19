931709e1c23dffa7e75195dfbebc328b2dbe48b5
leo <leo.legay85@gmail.com>
2025-12-19 18:43:12 +0100

    Première version de game_object pour tous les objets du jeu

--- project_name/Game_object.cpp
+++ project_name/Game_object.cpp
@@ -1,2 +1,12 @@
 #include "Game_object.h"
 
+Game_Object::Game_Object(std::array<std::array<bool, 6>, 8> tab, int x, int y):shape(tab),x(x),y(y){}
+
+Game_Object::~Game_Object(){
+
+}
+
+void Game_Object::setpos(int x, int y){
+  this->x = x;
+  this->y = y;
+}
--- project_name/Game_object.h
+++ project_name/Game_object.h
@@ -1,8 +1,17 @@
 #ifndef GAME_OBJECT_H_
 #define GAME_OBJECT_H_
 
+#include <array>
+
 class Game_Object{
-  
+  private:
+    std::array<std::array<bool, 6>, 8> shape;
+    int x;
+    int y;
+  public:
+    Game_Object(std::array<std::array<bool, 6>, 8> tab, int x, int y);
+    ~Game_Object();
+    void setpos(int x, int y);
 };
 
 #endif
