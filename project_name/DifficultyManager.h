1db3bf0df3e4b4ce355f8b922f5170c4238abe31
william <will.sean.woodward@gmail.com>
2026-01-16 10:54:53 +0100

    commentaire score et DifficultyManager

--- project_name/DifficultyManager.h
+++ project_name/DifficultyManager.h
@@ -10,12 +10,49 @@ class DifficultyManager{
         unsigned long harderdelay;
         int intensite;
     public:
+        /**
+         *@fn DifficultyManager()
+        *@brief initialise les valeurs de scores et les temps de rafraîchissement
+        */
         DifficultyManager();
+
+        /**
+         *@fn ~DifficultyManager()
+        *@brief destructeur de la classe Difficulty Manager
+        */
         ~DifficultyManager();
+        
+        /**
+         *@fn resetspawn()
+        *@brief Remet les valeurs initiales les variables de rapidités et la frequence de spawning des objets
+        */
         void resetspawn();
+
+        /**
+         *@fn harder()
+        *@brief method qui gère la difficulté du jeu en mettant à jour la rapidité et la frequence de spawning des objets
+        */
         void harder();
+
+        /**
+         *@fn getspawndelaymode1()
+        *@brief retourne la frequence d'apparition des objets pour le mode 1
+        *@return int
+        */
         const int getspawndelaymode1();
+
+        /**
+         *@fn getspawndelaymode2()
+        *@brief retourne la frequence d'apparition des objets pour le mode 2
+        *@return int
+        */
         const int getspawndelaymode2();
+
+        /**
+         *@fn getintensite()
+        *@brief retourne la rapidité de déplacement des objets
+        *@return int
+        */
         const int getintensite();
 };
 
--- project_name/score.h
+++ project_name/score.h
@@ -8,13 +8,55 @@ class Score {
   unsigned long scorerefreshing;
   unsigned lastime;
   public:
+
+  /**
+     *@fn Score()
+     *@brief initialise les valeurs de scores et les temps de rafraîchissement
+    */
   Score();
+
+  /**
+     *@fn updatescoreMode1()
+     *@brief met à jour le score dans le mode normal
+    */
   void updatescoreMode1();
+
+  /**
+     *@fn updatescoreMode2()
+     *@brief met à jour le score du mode 2 (le score augmente 3x plus que dans le mode 1)
+    */
   void updatescoreMode2();
+
+  /**
+     *@fn updatescorefinal()
+     *@brief met à jour le score final pour afficher correctement dans le GAMEOVER
+    */
   void updatescorefinal();
+
+  /**
+     *@fn resetscore()
+     *@brief remet à zero le score et met à jour le refraichissement pour relancer une partie
+    */
   void resetscore();
+
+  /**
+     *@fn actualiserlastime()
+     *@brief met à jour le temps de raifraichissement
+    */
   void actualiserlastime();
+
+  /**
+     *@fn getscore()
+     *@brief retourne le score
+     *@return renvoie un int
+    */
   int getscore();
+
+  /**
+     *@fn gethighscore()
+     *@brief retourne le score le plus haut (depuis la mise sous tension)
+     *@return renvoie un int
+    */
   int gethighscore();
 };
 
